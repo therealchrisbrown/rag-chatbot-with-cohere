6f0b03972e09fe22fe7a6d7699f8c367361ee83c
therealchrisbrown <ch.braun1@gmx.de>
2023-12-06 11:09:37 +0100

    building chat application with hugchat - added creds

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
+# Frontend libs
+import streamlit as st
+from hugchat import hugchat
+from hugchat.login import Login
+
+# .env libs
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# chatbot class import
 from chatbot import Chatbot
 from chatbot import Documents
 
+# Define sources
 sources = [
     {
         "title": "Text Embeddings", 
@@ -16,6 +28,38 @@ sources = [
         "url": "https://docs.cohere.com/docs/transformer-models"}   
 ]
 
+# Login class with Hugging Face credentials
+class Login(Login):
+    def __init__(self):
+        super().__init__(
+            username=os.environ["email_hug"],
+            password=os.environ["pw_hug"],
+            model="OpenAssistant/oasst-sft-6-llama-30b-xor",
+        )
+
+# Create a function that generates the chatbot response by calling the hugchat.generate_response method
+def generate_response(user_message):
+    response = hugchat.generate_response(
+        user_message,
+        login=Login(),
+        num_tokens=50,
+        temperature=0.9,
+        top_k=50,
+        top_p=0.9,
+        num_beams=5,
+    )
+    return response
+
+# Create a function that displays the chat messages in the streamlit app by calling the st.chat_message method
+def display_message(author, text):
+    st.chat_message(
+        author,
+        text,
+        color="#F63366" if author == "user" else "#0D6EFD",
+        avatar="👩🏻" if author == "user" else "🤖",
+        font_size=16,
+        border_radius=10,
+    )
 
 class App:
     def __init__(self, chatbot: Chatbot):
